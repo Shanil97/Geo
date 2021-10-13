4b2a200026097d562253c96d5ddd9e7f8b983c1c
Shanil97 <46552983+Shanil97@users.noreply.github.com>
2021-10-13 18:51:08 +0530

    Update st_first_app.py

--- st_first_app.py
+++ st_first_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 from streamlit_keplergl import keplergl_static
 
 
-df=pd.read_csv('D:\Intern\distribution optimization\intermediate_solution/score.csv')
+df=pd.read_csv('western.csv')
 # print('Shape=>',df.shape)
 # print(df.head())
 
