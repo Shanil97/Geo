ea3df9de4b2cf558e33f05f01b18457a81b13547
Shanil97 <46552983+Shanil97@users.noreply.github.com>
2021-10-13 17:20:04 +0530

    Add files via upload

---
+++ st_first_app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+from streamlit_keplergl import keplergl_static
+
+
+df=pd.read_csv('D:\Intern\distribution optimization\intermediate_solution/score.csv')
+# print('Shape=>',df.shape)
+# print(df.head())
+
+from keplergl import KeplerGl
+map1=KeplerGl(height=600)
+map1.add_data(data=df,name='western province')
+
+
+# display streamlit map
+keplergl_static(map1)
+
+# st.header("Interactive Widgets")
+# st.radio("Choose One", ["To be", "Not to be"])
+# st.slider("How old are you?", min_value=1, max_value=100)
+# st.date_input("Choose your favorite date")
